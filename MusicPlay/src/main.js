47ede9eb323f27c4b4fe795f045c3b46e41a7d86
Eslokaren <eslokarenalcantara@gmail.com>
2021-02-18 19:42:56 -0600

    Estructura inicial

---
+++ MusicPlay/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/js/brands.js'
+import '@fortawesome/fontawesome-free/js/solid.js'
+import '@fortawesome/fontawesome-free/js/fontawesome.js'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
